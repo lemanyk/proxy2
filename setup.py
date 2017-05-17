dc2b76ad4b179454973ef2c42f8c8e17490626bb
lemanyk <lemanyk@gmail.com>
2017-05-17 16:08:36 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='proxy2',
+    version='1.0',
+    description='HTTP/HTTPS proxy in a single python script',
+    author='inaz2',
+    author_email='',
+    url='https://github.com/inaz2/proxy2/',
+)
